7ae9440fbafd78e57b11e51262fd084b9a60d626
MaksimKrishtopa <k.maks21@mail.ru>
2023-11-16 18:58:39 +0700

    2.1 Сделано, без стилизации

--- design1/catalog/forms.py
+++ design1/catalog/forms.py
@@ -1,60 +1,63 @@
 # design1/catalog/forms.py
-
 import regex
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from .models import UserProfile
+from django.core import validators
+from django.core.exceptions import ValidationError
+from django.utils.deconstruct import deconstructible
 from unidecode import unidecode
+from .models import UserProfile
+
+
+@deconstructible
+class FIOValidator(validators.RegexValidator):
+    regex = r"^[а-яА-Я\s-]+$"
+    message = "Enter a valid Full Name. This value may contain only Cyrillic letters, spaces, and dashes."
+
+
+@deconstructible
+class LoginValidator(validators.RegexValidator):
+    regex = r"^[a-zA-Z-]+$"
+    message = "Enter a valid login. This value may contain only Latin letters and '-'."
 
 
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={'placeholder': 'Email'}))
-    agree_to_processing = forms.BooleanField(required=True,
-                                             widget=forms.CheckboxInput(attrs={'class': 'form-check-input'}))
+    agree_to_processing = forms.BooleanField(required=True, widget=forms.CheckboxInput(attrs={'class': 'form-check-input'}))
 
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2', 'agree_to_processing']
 
+    username = forms.CharField(max_length=30, required=True, widget=forms.TextInput(attrs={'placeholder': 'Username'}))
+
     def clean_username(self):
         username = self.cleaned_data['username']
+        validator = LoginValidator()
+        try:
+            validator(username)
+        except ValidationError:
+            raise forms.ValidationError(validator.message)
 
-        # Проверка на латиницу и дефис
-        if not username.encode('utf-8').isalpha() or '-' in username:
-            raise forms.ValidationError('Логин должен содержать только латинские буквы и цифры, без дефисов.')
-
-        # Проверка на уникальность логина
         if User.objects.filter(username=username).exists():
-            raise forms.ValidationError('Такой логин уже занят. Пожалуйста, выберите другой.')
+            raise forms.ValidationError('This username is already taken. Please choose another one.')
 
         return username
-
-    def clean_email(self):
-        email = self.cleaned_data['email']
-        # Валидация формата email-адреса
-        if '@' not in email or '.' not in email:
-            raise forms.ValidationError('Некорректный формат email-адреса.')
-        return email
-
-    def clean_fio(self):
-        fio = self.cleaned_data['fio']
-
-        # Проверка на кириллические буквы, дефис и пробелы
-        if not regex.match("^[А-Яа-яЁё\s-]+$", fio):
-            raise forms.ValidationError('ФИО должно содержать только кириллические буквы, дефис и пробелы.')
-
-        return fio
-
-    def clean_agree_to_processing(self):
-        agree_to_processing = self.cleaned_data['agree_to_processing']
-        # Проверка на согласие с обработкой персональных данных
-        if not agree_to_processing:
-            raise forms.ValidationError('Необходимо согласие с обработкой персональных данных.')
-        return agree_to_processing
 
 
 class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
         fields = ['fio']
+
+    fio = forms.CharField(max_length=255, required=True, widget=forms.TextInput(attrs={'placeholder': 'Full Name'}))
+
+    def clean_fio(self):
+        fio = self.cleaned_data['fio']
+        validator = FIOValidator()
+        try:
+            validator(fio)
+        except ValidationError:
+            raise forms.ValidationError(validator.message)
+        return fio
